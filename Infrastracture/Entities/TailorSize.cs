14b4f1fd9cb9f6424cc6bfb7f2729306fb5bc6c4
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-08-31 10:14:12 +0430

    tailor and your design entites created

---
+++ Infrastracture/Entities/TailorSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastracture.Entities
+{
+    public class TailorSize
+    {
+        [Key]
+        public int Id { get; set; }
+        public float Shane { get; set; }
+        public float Astin { get; set; }
+        public float Kamar { get; set; }
+        public float ZerBaghal { get; set; }
+        public float Shelwar { get; set; }
+        public float perahan { get; set; }
+        public string Detail { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
